ce58dfcfc26629c46713ac00620fb82ba552906e
Sunny-zz <b695340402@163.com>
2020-04-10 15:36:05 +0800

    update

---
+++ precache-manifest.953cc894ae7726adaf8a7ae8595e81f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca5a1a1629fed601fdc77d0a1af3941",
+    "url": "/react-shequ-demo/index.html"
+  },
+  {
+    "revision": "83a3a48c57f5f0b0098b",
+    "url": "/react-shequ-demo/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "2e9430d0e1341f336c94",
+    "url": "/react-shequ-demo/static/css/main.eac43d90.chunk.css"
+  },
+  {
+    "revision": "83a3a48c57f5f0b0098b",
+    "url": "/react-shequ-demo/static/js/2.0c6740e8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/react-shequ-demo/static/js/2.0c6740e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9430d0e1341f336c94",
+    "url": "/react-shequ-demo/static/js/main.d0da1e59.chunk.js"
+  },
+  {
+    "revision": "b0b1ebe3a2532e848bc4",
+    "url": "/react-shequ-demo/static/js/runtime-main.37b8f99b.js"
+  },
+  {
+    "revision": "e1ea82cb1c39656b925012efe60f22ea",
+    "url": "/react-shequ-demo/static/media/vue.e1ea82cb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.22ce9e9c7af5cacebe1be93d9cdcce8e.js"
+  "/react-shequ-demo/precache-manifest.953cc894ae7726adaf8a7ae8595e81f8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-shequ-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
